313611b47024718ba3c29c3f346eadd62a91ff81
Rachel Williams <rachelwilliams@MacBook-Pro.local>
2020-02-08 14:56:02 -0600

    Updates

---
+++ precache-manifest.d4c2c937866b98cd9d1eb786fe48b0fc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd479b4929a18ca73b6437153c96db15",
+    "url": "/weatherapp/index.html"
+  },
+  {
+    "revision": "287da3ab22a4a8d84031",
+    "url": "/weatherapp/static/css/main.dbe2609e.chunk.css"
+  },
+  {
+    "revision": "8a21998d9ad6e8f7c109",
+    "url": "/weatherapp/static/js/2.80d3a92f.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/weatherapp/static/js/2.80d3a92f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "287da3ab22a4a8d84031",
+    "url": "/weatherapp/static/js/main.4563283c.chunk.js"
+  },
+  {
+    "revision": "442fbc281f2b59494b06",
+    "url": "/weatherapp/static/js/runtime-main.c648e4b6.js"
+  }
+]);
